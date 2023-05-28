8a2145f3116fb559c1af502465e2c8bd06f97dc8
Danilo <daniloassis.ti@gmail.com>
2023-05-28 20:36:26 -0300

    feat(api): methods fns

---
+++ src/mocks/indicationMock.ts
@@ -0,0 +1,18 @@
+export const indicationsMock = [
+  {
+    id: 1,
+    personalName: "John Doe",
+    block: "A",
+    ap: 101,
+    indicationCategory: "Plumbing",
+    value: 50,
+  },
+  {
+    id: 2,
+    personalName: "Jane Smith",
+    block: "B",
+    ap: 202,
+    indicationCategory: "Electrician",
+    value: 75,
+  },
+];
