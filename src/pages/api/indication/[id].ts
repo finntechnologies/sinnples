38d17d749bf8ed6e391a95fa63267a1019237107
Danilo <daniloassis.ti@gmail.com>
2023-05-28 20:36:26 -0300

    feat(api): methods fns

--- src/pages/api/indication/[id].ts
+++ src/pages/api/indication/[id].ts
@@ -1,16 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { IIndication } from "../../../modules/IndicationModel";
-import { indicationsMock } from "./index";
+import Indication, { IIndication } from "../../../modules/IndicationModel";
+import { connectToDatabase } from "../../../db/connectDatabase";
 
-export default function indicationHandler(
+const handleIndicationGet = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  await connectToDatabase();
+
+  const { id } = req.query;
+
+  if (!id) {
+    res.status(404).json({ message: `Missing indication id` });
+  }
+
+  const indication = await Indication.findOne({
+    _id: id,
+  });
+
+  res.status(200).json(indication);
+};
+
+const indicationHandler = async (
   req: NextApiRequest,
   res: NextApiResponse<IIndication>
-) {
-  const { query } = req;
-  const { id } = query;
-  const indication = indicationsMock.find((i) => i.id === id);
+) => {
+  await handleIndicationGet(req, res);
+};
 
-  return indication
-    ? res.status(200).json(indication)
-    : res.status(404).json({ message: `Indication with id: ${id} not found.` });
-}
+export default indicationHandler;
--- src/pages/api/indication/index.ts
+++ src/pages/api/indication/index.ts
@@ -2,41 +2,102 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../../../db/connectDatabase";
 import Indication from "../../../modules/IndicationModel";
 
-export const indicationsMock = [
-  {
-    id: 1,
-    personalName: "John Doe",
-    block: "A",
-    ap: 101,
-    indicationCategory: "Plumbing",
-    value: 50,
-  },
-  {
-    id: 2,
-    personalName: "Jane Smith",
-    block: "B",
-    ap: 202,
-    indicationCategory: "Electrician",
-    value: 75,
-  },
-];
-
-const handleIndicationGetAll = async () => {
+const handleIndicationGetAll = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
   await connectToDatabase();
 
-  const indications = await Indication.find();
+  const indications = await Indication.find({});
 
-  return { indications: [...indications, ...indicationsMock] };
+  res.status(200).json(indications);
 };
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const { indications } = await handleIndicationGetAll();
+const handleIndicationAdd = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  // @todo validate body with yup or zod
+  const body = JSON.parse(req.body);
+  const indication = await new Indication(body).save();
 
-    res.status(200).json(indications);
+  return res.status(200).json(indication);
+};
+
+const handleIndicationUpdate = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  await connectToDatabase();
+
+  const { id } = req.query;
+  const { name, apartmentBlock, apartment } = req.body;
+
+  const updatedIndication = {
+    name,
+    apartmentBlock,
+    apartment,
+  };
+
+  const indication = await Indication.findOneAndUpdate(
+    { _id: id },
+    { $set: updatedIndication },
+    { new: true }
+  );
+
+  if (!indication) {
+    res.status(404).json({ message: `Indication with ${id} not found` });
+  } else {
+    res.status(200).json(indication);
+  }
+};
+
+const handleIndicationDelete = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const { query } = req;
+  const { id } = query;
+
+  const indication = await Indication.findOneAndDelete({
+    _id: id,
+  });
+
+  if (!indication) {
+    return res
+      .status(404)
+      .json({ message: `Indication with id: ${id} not found.` });
+  }
+
+  return res
+    .status(200)
+    .json({ message: `Indication with id deleted successfully` });
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { method } = req;
+
+    switch (method) {
+      case "GET":
+        await handleIndicationGetAll(req, res);
+        break;
+      case "POST":
+        await handleIndicationAdd(req, res);
+        break;
+      case "PUT":
+        await handleIndicationUpdate(req, res);
+        break;
+      case "DELETE":
+        await handleIndicationDelete(req, res);
+        break;
+      default:
+        res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
   } catch (error) {
-    console.error("Error fetching indications:", error);
-    res.status(500).json({ message: "Error fetching indications" });
+    console.error("Error handling service request:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
