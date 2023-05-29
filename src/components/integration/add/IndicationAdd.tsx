db97bb94a426e2c88b5af5951f8350b8c8964874
Danilo <daniloassis.ti@gmail.com>
2023-05-28 22:42:34 -0300

    feat(add): add

--- src/components/integration/add/IndicationAdd.tsx
+++ src/components/integration/add/IndicationAdd.tsx
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import Layout from "../../Layout";
+import {useRouter} from "next/router";
 
 const IndicationAdd = () => {
+  const router = useRouter();
   const [formData, setFormData] = useState({
-    name: "",
+    firstName: "",
+    lastName: "",
     apartmentBlock: "",
     apartment: "",
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const { firstName, lastName, ...restBody } = formData;
+    const body = {
+      ...restBody,
+      name: `${firstName} ${lastName}`,
+    };
+
     try {
       const response = await fetch("/api/indication", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(body),
       });
 
       if (response.ok) {
         // @todo tailwind feedback
         console.log("Indication saved successfully");
+        return router.push('/');
       } else {
         // @todo tailwind feedback
         console.log("Error saving indication");
@@ -42,51 +53,121 @@ const IndicationAdd = () => {
   return (
     <Layout>
       <div className="container mx-auto">
-        <h1 className="text-2xl font-bold mb-4">
-          Nova Indicação
-        </h1>
-        <form onSubmit={handleSubmit} className="max-w-md">
-          <div className="mb-4">
-            <label className="block mb-2 font-medium" htmlFor="name">
-              Nome
-            </label>
-            <input
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
+        <h1 className="text-2xl font-bold mb-4">Nova Indicação</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-12">
+            <div className="border-b border-gray-900/10 pb-12">
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Indicações
+              </h2>
+              <p className="mt-1 text-sm leading-6 text-gray-600">
+                Essas informações serão exibidas publicamente, portanto, tenha
+                cuidado com o que será compartilhado.
+              </p>
+            </div>
+
+            <div className="border-b border-gray-900/10 pb-12">
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Informações Pessoais
+              </h2>
+
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="firstName"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Nome
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="firstName"
+                      id="firstName"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="lastName"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Sobrenome
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="lastName"
+                      id="lastName"
+                      autoComplete="family-name"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="apartmentBlock"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Bloco do Apartamento
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="apartmentBlock"
+                      id="apartmentBlock"
+                      value={formData.apartmentBlock}
+                      onChange={handleChange}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="apartment"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Apartamento
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="apartment"
+                      id="apartment"
+                      value={formData.apartment}
+                      onChange={handleChange}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="mb-4">
-            <label className="block mb-2 font-medium" htmlFor="apartmentBlock">
-              Bloco do Apartamento
-            </label>
-            <input
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-              type="text"
-              id="apartmentBlock"
-              name="apartmentBlock"
-              required
-            />
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Indicar
+            </button>
           </div>
-          <div className="mb-4">
-            <label className="block mb-2 font-medium" htmlFor="apartment">
-              Apartmento
-            </label>
-            <input
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-              type="text"
-              id="apartment"
-              name="apartment"
-              required
-            />
-          </div>
-          <button className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
-            Indicar
-          </button>
         </form>
       </div>
     </Layout>
