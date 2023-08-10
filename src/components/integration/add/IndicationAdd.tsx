52971b03b34302eb442a70890337d0a84ecaf225
Thiago <lucimara1422@gmail.com>
2023-08-10 15:58:57 -0300

    feat(form) add react-hook-form + zod

--- src/components/integration/add/IndicationAdd.tsx
+++ src/components/integration/add/IndicationAdd.tsx
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../../Layout";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
+import { z } from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type InfoTableSchemaData = z.infer<typeof InfoTableSchema>
+
+const InfoTableSchema = z.object({
+  firstName: z.string().min(3, "Deve conter no minimo 3 caracteres"),
+  lastName: z.string().nonempty("Obrigatório"),
+  apartmentBlock: z.string().nonempty("Obrigatório"),
+  apartment: z.string().nonempty("Obrigatório"),
+  cep: z.string().min(8, "Deve conter no minimo 8 caracteres").nonempty("Obrigatório")
+})
 
 const IndicationAdd = () => {
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<InfoTableSchemaData>({
+    resolver: zodResolver(InfoTableSchema),
+  })
+
   const router = useRouter();
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    apartmentBlock: "",
-    apartment: "",
-  });
-
-  const handleSubmit = async (e: React.FormEvent<HTMLInputElement>) => {
-    e.preventDefault();
-
-    const { firstName, lastName, ...restBody } = formData;
+  const handleInfo = async (data: InfoTableSchemaData ) => {
+    const { firstName, lastName, ...restBody } = data;
     const body = {
       ...restBody,
       name: `${firstName} ${lastName}`,
@@ -43,18 +55,13 @@ const IndicationAdd = () => {
     }
   };
 
-  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [e.target.value]: e.target.value,
-    }));
-  };
+  
 
   return (
     <Layout>
       <div className="container mx-auto">
         <h1 className="text-2xl font-bold mb-4">Nova Indicação</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(handleInfo)}>
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-12">
               <h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -81,14 +88,18 @@ const IndicationAdd = () => {
                   </label>
                   <div className="mt-2">
                     <input
+                      {...register("firstName")}
                       type="text"
                       name="firstName"
                       id="firstName"
-                      value={formData.firstName}
-                      onChange={handleChange}
                       autoComplete="given-name"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
+                    {errors.firstName && (
+                      <span className="text-sm text-red-600 ">
+                      {errors.firstName.message}
+                    </span>
+                    )}
                   </div>
                 </div>
 
@@ -101,14 +112,18 @@ const IndicationAdd = () => {
                   </label>
                   <div className="mt-2">
                     <input
+                      {...register("lastName")}
                       type="text"
                       name="lastName"
                       id="lastName"
                       autoComplete="family-name"
-                      value={formData.lastName}
-                      onChange={handleChange}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
+                    {errors.lastName && (
+                      <span className="text-sm text-red-600 ">
+                      {errors.lastName.message}
+                    </span>
+                    )}
                   </div>
                 </div>
               </div>
@@ -122,13 +137,17 @@ const IndicationAdd = () => {
                   </label>
                   <div className="mt-2">
                     <input
+                      {...register("apartmentBlock")}
                       type="text"
                       name="apartmentBlock"
                       id="apartmentBlock"
-                      value={formData.apartmentBlock}
-                      onChange={handleChange}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
+                    {errors.apartmentBlock && (
+                      <span className="text-sm text-red-600 ">
+                      {errors.apartmentBlock.message}
+                    </span>
+                    )}
                   </div>
                 </div>
 
@@ -141,13 +160,39 @@ const IndicationAdd = () => {
                   </label>
                   <div className="mt-2">
                     <input
+                    {...register("apartment")}
                       type="text"
                       name="apartment"
                       id="apartment"
-                      value={formData.apartment}
-                      onChange={handleChange}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
+                    {errors.apartment && (
+                      <span className="text-sm text-red-600 ">
+                      {errors.apartment.message}
+                    </span>
+                    )}
+                  </div>
+                </div>
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="apartment"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Cep
+                  </label>
+                  <div className="mt-2">
+                    <input
+                    {...register("cep")}
+                      type="text"
+                      name="cep"
+                      id="cep"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                    {errors.cep && (
+                      <span className="text-sm text-red-600 ">
+                      {errors.cep.message}
+                    </span>
+                    )}
                   </div>
                 </div>
               </div>
