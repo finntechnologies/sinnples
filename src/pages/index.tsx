3162491ef8454c85765cdc3f21353a150d17d361
Thiago <lucimara1422@gmail.com>
2023-08-14 19:10:42 -0300

    feat(layout) root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,10 @@
-import IndicationList from "../components/integration/list/IndicationList";
-import useSwr from "swr";
 import Layout from "../components/Layout";
+import Dashboard from "../components/integration/list/Dashboard";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
-const Index = () => {
-  const {
-    data: indications,
-    error,
-    isLoading,
-  } = useSwr("/api/indication", fetcher);
-
-  if (error) return <div>Falha ao carregar</div>;
-
-  if (isLoading) return <div>Carregando...</div>;
-
-  if (!indications) return null;
-
+const Index = () => { 
   return (
     <Layout>
-      <IndicationList indications={indications} />
+      <Dashboard />
     </Layout>
   );
 };
