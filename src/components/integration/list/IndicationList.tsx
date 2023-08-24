fb86c7d68144aca4a36967078f1ac559ee38e80e
Thiago <lucimara1422@gmail.com>
2023-08-24 18:55:35 -0300

    feat(responsive) layout improve

--- src/components/integration/list/IndicationList.tsx
+++ src/components/integration/list/IndicationList.tsx
@@ -21,8 +21,8 @@ const IndicationList = ({ indications = [], search }: IntegrationListProps) => {
   const renderRows = () => {
     if (!indications || !indications.length) {
       return (
-        <div className="flex items-center justify-center rounded-2xl">
-        <div className="flex items-center justify-center mb-4 mt-10 border-2 border-gray-600 h-60 w-96 rounded-2xl">
+        <div className="flex items-center justify-center rounded-2xl w-screen ">
+        <div className="flex items-center justify-center mb-4 mt-10 h-60 w-96 rounded-2xl sm:border-2 sm:border-gray-600 sm:rounded-2xl">
           <h2 className="text-lg font-semibold text-gray-200">
           Comece agora e indique algo! 🚀
           </h2>
@@ -32,9 +32,12 @@ const IndicationList = ({ indications = [], search }: IntegrationListProps) => {
     }
 
     return FilteredData?.map((indication) => {
-      return <IndicationListRow key={indication._id} indication={indication} />;
+      return  <IndicationListRow key={indication._id} indication={indication} />
+      
     });
   };
+
+  
 
   return (
     <>
---
+++ src/components/integration/list/IndicationTable.tsx
@@ -0,0 +1,57 @@
+interface props {
+  indications: any
+}
+
+const IndicationTable = ({ indications }: props) => {
+    return (
+      <thead className={`bg-gray-300 dark:bg-gray-700  ${indications.length === 0 ? 'hidden' : ''}`}>
+        <tr>
+          <th scope="col" className="p-4">
+            <div className="flex items-center">
+              <input
+                id="checkbox-all"
+                aria-describedby="checkbox-1"
+                type="checkbox"
+                className="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
+              />
+              <label htmlFor="checkbox-all" className="sr-only">
+                checkbox
+              </label>
+            </div>
+          </th>
+          <th
+            scope="col"
+            className="px-20  py-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+          >
+            Nome
+          </th>
+          <th
+            scope="col"
+            className="px-20 py-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+          >
+            Categoria
+          </th>
+          <th
+            scope="col"
+            className="px-20 py-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+          >
+            Bloco do AP
+          </th>
+          <th
+            scope="col"
+            className="px-20 py-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+          >
+            Apartamento
+          </th>
+          <th
+            scope="col"
+            className="px-16 py-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+          >
+            Cep
+          </th>
+        </tr>
+      </thead>
+    )
+  }
+  
+  export default IndicationTable
