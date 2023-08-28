c4f7f6f73e56ed748db3f6743639a1c36c17c540
Thiago <lucimara1422@gmail.com>
2023-08-28 17:17:50 -0300

    feat(locale) add type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,12 @@ import Layout from "../components/Layout";
 import Dashboard from "../components/integration/list/Dashboard";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+interface LocaleProps {
+  locale: string
+}
+
 const Index = () => { 
-  return (
+    return (
     <Layout>
        <Dashboard />  
       </Layout>
@@ -11,7 +15,7 @@ const Index = () => {
 };
 
 
-export async function getStaticProps({ locale }) {
+export async function getStaticProps({ locale }: LocaleProps) {
   return {
     props: {
       ...(await serverSideTranslations(locale, [
